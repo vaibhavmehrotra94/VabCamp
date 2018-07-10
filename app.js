2d1dc2d4811e5cb1ad3d9e074512577ae21ac7d5
Vaibhav Mehrotra <31144001+vaibhavmehrotra94@users.noreply.github.com>
2018-07-10 23:43:46 +0530

    Second Version
    Added New pages plus the Database Facility for storing things

--- app.js
+++ app.js
@@ -2,18 +2,29 @@ var express = require('express');
 // var request = require('request');
 var app = express();
 var bodyParser = require('body-parser');
-var location = [{name:"Location-1", image:"https://www.melrimbagarden.com/sites/default/files/images/DSC04388.JPG"},
-                {name:"Location-2", image:"https://media-cdn.tripadvisor.com/media/photo-s/01/87/c4/7d/elkwood-camp-ground.jpg"},
-                {name:"Location-3", image:"http://www.visitvictoria.com/-/media/images/high-country/things-to-do/outdoor-activities/camping-falls-to-hotham-alpine-crossing_hc_r_1461477_1150x863.jpg?ts=20151020370426&amp;cp=95&w=480&h=360&crop=1"},
-                {name:"Location-4", image:"https://www.melrimbagarden.com/sites/default/files/images/DSC04388.JPG"},
-                {name:"Location-5", image:"https://media-cdn.tripadvisor.com/media/photo-s/01/87/c4/7d/elkwood-camp-ground.jpg"},
-                {name:"Location-6", image:"http://www.visitvictoria.com/-/media/images/high-country/things-to-do/outdoor-activities/camping-falls-to-hotham-alpine-crossing_hc_r_1461477_1150x863.jpg?ts=20151020370426&amp;cp=95&w=480&h=360&crop=1"},
-                {name:"Location-7", image:"https://www.melrimbagarden.com/sites/default/files/images/DSC04388.JPG"},
-                {name:"Location-8", image:"https://media-cdn.tripadvisor.com/media/photo-s/01/87/c4/7d/elkwood-camp-ground.jpg"},
-                {name:"Location-9", image:"http://www.visitvictoria.com/-/media/images/high-country/things-to-do/outdoor-activities/camping-falls-to-hotham-alpine-crossing_hc_r_1461477_1150x863.jpg?ts=20151020370426&amp;cp=95&w=480&h=360&crop=1"},
-                {name:"Location-10", image:"https://www.melrimbagarden.com/sites/default/files/images/DSC04388.JPG"},
-                {name:"Location-11", image:"https://media-cdn.tripadvisor.com/media/photo-s/01/87/c4/7d/elkwood-camp-ground.jpg"},
-                {name:"Location-12", image:"http://www.visitvictoria.com/-/media/images/high-country/things-to-do/outdoor-activities/camping-falls-to-hotham-alpine-crossing_hc_r_1461477_1150x863.jpg?ts=20151020370426&amp;cp=95&w=480&h=360&crop=1"}];
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var campGround = mongoose.model("campground", campgroundSchema);
+
+
+// var location = [{name:"Location-1", image:"https://www.melrimbagarden.com/sites/default/files/images/DSC04388.JPG"},
+//                 {name:"Location-2", image:"https://media-cdn.tripadvisor.com/media/photo-s/01/87/c4/7d/elkwood-camp-ground.jpg"},
+//                 {name:"Location-3", image:"http://www.visitvictoria.com/-/media/images/high-country/things-to-do/outdoor-activities/camping-falls-to-hotham-alpine-crossing_hc_r_1461477_1150x863.jpg?ts=20151020370426&amp;cp=95&w=480&h=360&crop=1"},
+//                 {name:"Location-4", image:"https://www.melrimbagarden.com/sites/default/files/images/DSC04388.JPG"},
+//                 {name:"Location-5", image:"https://media-cdn.tripadvisor.com/media/photo-s/01/87/c4/7d/elkwood-camp-ground.jpg"},
+//                 {name:"Location-6", image:"http://www.visitvictoria.com/-/media/images/high-country/things-to-do/outdoor-activities/camping-falls-to-hotham-alpine-crossing_hc_r_1461477_1150x863.jpg?ts=20151020370426&amp;cp=95&w=480&h=360&crop=1"},
+//                 {name:"Location-7", image:"https://www.melrimbagarden.com/sites/default/files/images/DSC04388.JPG"},
+//                 {name:"Location-8", image:"https://media-cdn.tripadvisor.com/media/photo-s/01/87/c4/7d/elkwood-camp-ground.jpg"},
+//                 {name:"Location-9", image:"http://www.visitvictoria.com/-/media/images/high-country/things-to-do/outdoor-activities/camping-falls-to-hotham-alpine-crossing_hc_r_1461477_1150x863.jpg?ts=20151020370426&amp;cp=95&w=480&h=360&crop=1"},
+//                 {name:"Location-10", image:"https://www.melrimbagarden.com/sites/default/files/images/DSC04388.JPG"},
+//                 {name:"Location-11", image:"https://media-cdn.tripadvisor.com/media/photo-s/01/87/c4/7d/elkwood-camp-ground.jpg"},
+//                 {name:"Location-12", image:"http://www.visitvictoria.com/-/media/images/high-country/things-to-do/outdoor-activities/camping-falls-to-hotham-alpine-crossing_hc_r_1461477_1150x863.jpg?ts=20151020370426&amp;cp=95&w=480&h=360&crop=1"}];
                     
 
 app.set("view engine", "ejs");
@@ -27,15 +38,30 @@ app.get("/",function(req, res) {
 
 
 app.get("/campgrounds",function(req, res) {
-    res.render('campgrounds',{location: location});
+    campGround.find({}, function(err, camp){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('campgrounds',{location: camp});
+        }
+    });
+    
 });
 
 app.post("/campgrounds",function(req,res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampLocation = {name: name, image: image};
-    location.push(newCampLocation);
-    res.redirect("/campgrounds");
+    
+    campGround.create(newCampLocation, function(err, campgrounds){
+        if(err){
+            console.log("ERROR!!");
+            console.log(err);
+        }else{
+            console.log(campgrounds);
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new",function(req,res){
